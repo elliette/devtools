7b633baa613caec0288edee28b7944a617e0fad0
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-08-22 16:47:27 -0700

    Messy state

--- packages/devtools_app/webdriver_test/web_compiler_test.dart
+++ packages/devtools_app/webdriver_test/web_compiler_test.dart
@@ -14,62 +14,25 @@ import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 import 'package:webdriver/async_io.dart';
 
-// Note: Copy the server logic from 
+import '../integration_test/test_infra/run/run_test.dart';
+
+// Note: Copy the server logic from
 // devtools_app/integration_test/test_infra/run/run_test.dart instead
 void main() {
   late Process devtoolsProcess;
   late WebDriver driver;
-  final devtoolsUrlCompleter = Completer<String>();
 
   setUp(() async {
     final chromedriver = ChromeDriver();
     await chromedriver.start(debugLogging: true);
 
-    // Start the DevTools server.
-    // This command is equivalent to running `dt serve` from the repository root.
-    // We run it from the `devtools_tool` directory to ensure the relative paths
-    // in the tool resolve correctly.
-    final devtoolsToolPath = path.join(
-      Directory.current.path,
-      '..',
-      '..',
-      'tool',
+    print('START DEVTOOLS PROCESS');
+    devtoolsProcess = await _startLocalDevToolsServer();
+    final devToolsServerAddress = await listenForDevToolsAddress(
+      devtoolsProcess,
+      timeout: const Duration(minutes: 3),
     );
-    print('DEVTOOLS TOOL PATH $devtoolsToolPath');
-
-    devtoolsProcess = await Process.start('dart', [
-      'run',
-      'bin/dt.dart',
-      'serve',
-    ], workingDirectory: devtoolsToolPath);
-
-    // Wait for the server to be ready by listening to its output.
-    devtoolsProcess.stdout
-        .transform(utf8.decoder)
-        .transform(const LineSplitter())
-        .listen((line) {
-          print(line);
-          const _devToolsServerAddressLine = 'Serving DevTools at ';
-          if (line.contains(_devToolsServerAddressLine)) {
-            // This will pull the server address from a String like:
-            // "Serving DevTools at http://127.0.0.1:9104.".
-            final regexp = RegExp(
-              r'http:\/\/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+',
-            );
-            final match = regexp.firstMatch(line);
-            print('!!!!!!!!! A MATCH');
-            if (match != null && !devtoolsUrlCompleter.isCompleted) {
-              print('URL IS ${match.group(0)}');
-              devtoolsUrlCompleter.complete(match.group(0));
-              // addressCompleter.complete();
-            }
-          }
-        });
-
-    final devtoolsUrl = await devtoolsUrlCompleter.future.timeout(
-      const Duration(minutes: 2),
-      onTimeout: () => throw 'DevTools server did not start within 2 minutes.',
-    );
+    print('====== DEVTOOLS SERVER ADDRESS IS ==== $devToolsServerAddress');
 
     // Create a WebDriver instance.
     // This requires a running chromedriver instance.
@@ -80,7 +43,8 @@ void main() {
     );
 
     // Navigate to the DevTools URL.
-    await driver.get(devtoolsUrl);
+    await driver.get(devToolsServerAddress);
+    print('DONE NAVIGATING');
   });
 
   tearDown(() async {
@@ -92,7 +56,25 @@ void main() {
     test('loads the app and has the correct title', () async {
       // Verify that the app has loaded by checking the title.
       final title = await driver.title;
-      expect(title, contains('DevTools'));
+      // print('======= TITLE IS $title');
+      // expect(title, contains('DevTools'));
     });
   });
+}
+
+Future<Process> _startLocalDevToolsServer() async {
+  final devtoolsToolPath = path.join(
+    Directory.current.path,
+    '..',
+    '..',
+    'tool',
+  );
+
+  final devtoolsProcess = await Process.start('dart', [
+    'run',
+    'bin/dt.dart',
+    'serve',
+    '--no-launch-browser',
+  ], workingDirectory: devtoolsToolPath);
+  return devtoolsProcess;
 }
